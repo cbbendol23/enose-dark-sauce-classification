094187fcbfe1a05cc292695649c15b5f590839ed
Cherry Ben Jr. R. Bendol <cbbendol23@gmail.com>
2025-09-10 13:12:33 +0800

    gumagana na

--- integration/workingapp copy.py
+++ integration/workingapp copy.py
@@ -43,7 +43,7 @@ RESULTFONT = ("Segoe UI", 30, "bold")
 SENSORFONT = ("Segoe UI", 13, "bold")
 
 # ---------------- SERIAL PORT MANAGER ---------------- #
-def open_serial(port="COM3", baud=9600):
+def open_serial(port="/dev/ttyACM0", baud=9600):
     for attempt in range(5):
         try:
             ser = serial.Serial(port, baud, timeout=1)
@@ -209,7 +209,7 @@ class ClassificationReadingPage(tk.Frame):
         self.update_sensor_display()
         self.update_timer(controller)
 
-    def gather_data(self, filename="integration/gathered_data.csv", port="COM3", baud=9600):
+    def gather_data(self, filename="integration/gathered_data.csv", port="/dev/ttyACM0", baud=9600):
         try:
             self.ser = open_serial(port, baud)
             if not self.ser:
@@ -419,7 +419,7 @@ class ExhaustPage(tk.Frame):
         self.update_sensor_display()
         self.update_timer(controller)
 
-    def gather_data(self, port="COM3", baud=9600):
+    def gather_data(self, port="/dev/ttyACM0", baud=9600):
         try:
             self.ser = open_serial(port, baud)
             if not self.ser:
